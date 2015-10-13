ba1c485e449e767f969a1b90230a8a1d07c2c350
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-13 11:37:50 -0700

    Added jbuilders for projects JSON API

---
+++ app/views/api/projects/_project.json.jbuilder
@@ -0,0 +1,4 @@
+json.extract!(
+  project,
+  :id, :user_id, :title, :description, :ord, :created_at
+)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'static_pages#root'
+  get 'static_pages/root'
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-  get 'static_pages/root'
+  namespace :api, defaults: { format: :json } do
+    resources :projects, only: [:create, :index, :show, :update, :destroy]
+  end
 end
