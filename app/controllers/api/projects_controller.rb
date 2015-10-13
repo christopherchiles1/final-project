b3e5ae4631c01885e118c15fb0d45f156eca3f6d
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-13 11:24:49 -0700

    Wrote CRUD for Projects controller

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,2 +1,41 @@
 class Api::ProjectsController < ApplicationController
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      render json: @project
+    else
+      render json: @project.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @projects = Project.all
+    render json: @projects
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    render json: @project
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      render json: @project
+    else
+      render json: @project.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    render json: @project
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :ord)
+  end
 end
