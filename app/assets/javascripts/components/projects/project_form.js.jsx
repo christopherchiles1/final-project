944b8da5849afb8b7a297b3d459f85e161febb92
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-14 11:53:55 -0700

    Added project form (used React.addons.LinkedStateMixin)

--- app/assets/javascripts/components/projects/project_form.js.jsx
+++ app/assets/javascripts/components/projects/project_form.js.jsx
@@ -2,12 +2,37 @@
   'use strict';
 
   root.ProjectForm = React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
+
+    getInitialState: function () {
+      return {
+        title: '',
+        description: ''
+      };
+    },
+
     render: function () {
       return (
-        <div className="project-form">
-        </div>
+        <form className="new-project">
+          <div>
+            <label htmlFor="title">Title: </label>
+            <input
+              type="text"
+              id="title"
+              valueLink={this.linkState("title")}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description: </label>
+            <input
+              type="text"
+              id="description"
+              valueLink={this.linkState("description")}
+            />
+          </div>
+          <button>Add Project</button>
+        </form>
       );
     }
   });
-
 }(this));
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Allow react addons!!!
+  config.react.addons = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
