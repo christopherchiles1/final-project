60483b5a1b44bbefdd8921cf11a25359f5cce963
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-19 14:56:43 -0700

    Navbar cosmetics

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -8,8 +8,41 @@
 
     render: function () {
       return (
-        <div className="navbar-div shadowed">
-        </div>
+        <nav className="navbar navbar-inverse custom">
+          <div className="container-fluid custom">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed custom" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+                <span className="glyphicon glyphicon-chevron-down"></span>
+              </button>
+              <img src="/assets/logo(small).png" />
+            </div>
+
+            <div className="collapse navbar-collapse custom" id="navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right custom">
+                <li className="dropdown custom">
+                  <a href="#"
+                    className="dropdown-toggle"
+                    data-toggle="dropdown"
+                    role="button" aria-haspopup="true" aria-expanded="false">
+                    {window.CURRENT_USER_USERNAME}
+                    <span className="caret"></span>
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a href="#">Sign Out</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="navbar-form navbar-right custom" role="search">
+                <div className="form-group group custom">
+                  <input type="text" placeholder="Search Tasks" />
+                  <button type="submit">
+                    <span className="glyphicon glyphicon-search"></span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </nav>
       );
     }
   });
