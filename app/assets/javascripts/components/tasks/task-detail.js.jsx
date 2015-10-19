538e55c8babe890b5683cb66482ac5ff353d9a0f
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-19 11:19:03 -0700

    Work on task detail view

--- app/assets/javascripts/components/tasks/task-detail.js.jsx
+++ app/assets/javascripts/components/tasks/task-detail.js.jsx
@@ -2,15 +2,31 @@
   'use strict';
 
   root.TaskDetail = React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
+
     getInitialState: function () {
-      return {
-        todos: TodoStore.taskTodos(this.props.task)
-      };
+      var initialState;
+      if (this.props.task) {
+        initialState = {
+          title: this.props.task.title,
+          description: this.props.task.description,
+          deadline: this.props.task.deadline
+        };
+      } else {
+        initialState = {
+          title: '', description: '', deadline: ''
+        };
+      }
+
+      return $.extend(initialState, {
+        todos: TodoStore.taskTodos(this.props.task),
+      });
     },
 
     componentDidMount: function () {
       TodoStore.addTodoChangeListener(this._onChange);
       TodoActions.fetchTaskTodos(this.props.task);
+      this.refs.titleInput.getDOMNode().focus();
     },
 
     componentWillUnmount: function () {
@@ -33,11 +49,28 @@
       //   todoList = <div className="todos-list empty"></div>;
       // }
       return (
-        <div className="task-detail hover-options">
-          {this.props.task.title}
-          <span className="glyphicon glyphicon-chevron-up pull-right hover-option"
+        <div className="task-detail">
+          <span className="glyphicon glyphicon-chevron-up pull-right"
             aria-hidden="true"
             onClick={this.props.toggleDetail}></span>
+          <form>
+            <div>
+              <input className="task-detail-input title"
+                ref="titleInput"
+                placeholder="Task Title"
+                type="text"
+                valueLink={this.linkState("title")}
+              />
+            </div>
+            <div>
+              <input className="task-detail-input description"
+                rows="5"
+                placeholder="Task Description"
+                type="text"
+                valueLink={this.linkState("description")}
+              />
+            </div>
+          </form>
         </div>
       );
     }
