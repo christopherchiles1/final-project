dc3acc3af8bfd138b8becfbf7eebeeb513feaac3
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-21 18:21:24 -0700

    CSS improvements to navbar

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -2,44 +2,39 @@
   'use strict';
 
   root.Navbar = React.createClass({
-    _logoutUser: function () {
+    logoutUser: function () {
       ApiActions.logoutUser();
     },
 
     render: function () {
       return (
-        <nav className="navbar navbar-inverse custom">
-          <div className="container-fullwidth custom">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed custom"
-                data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-                <span className="glyphicon glyphicon-chevron-down"></span>
-              </button>
-              <img src="logo(small).png" />
-            </div>
+        <nav className="lt-navbar">
+          <div className="lt-navbar-left">
+            <img className="lt-logo" src="logo(small).png" />
+          </div>
 
-            <div className="collapse navbar-collapse custom" id="navbar-collapse-1">
-              <ul className="nav navbar-nav navbar-right custom">
-                <li className="dropdown custom">
-                  <a className="dropdown-toggle"
-                    data-toggle="dropdown"
-                    role="button" aria-haspopup="true" aria-expanded="false">
-                    {window.CURRENT_USER_USERNAME}
-                    <span className="caret"></span>
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li><a onClick={ApiUtil.logoutUser}>Log out</a></li>
-                  </ul>
-                </li>
+          <div className="lt-navbar-left">
+            <form className="lt-navbar-form">
+              <div className="lt-form-group group">
+                <input type="text" placeholder="Search Tasks" />
+                <button type="submit">
+                  <span className="glyphicon glyphicon-search"></span>
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div className="lt-navbar-right">
+            <div className="dropdown lt-navbar-dropdown">
+              <div className="dropdown-toggle"
+                data-toggle="dropdown">
+                {"Profile "}
+                <span className="caret"></span>
+              </div>
+              <ul className="dropdown-menu dropdown-menu-right">
+                <li className="dropdown-label">Logged in as {window.CURRENT_USER_USERNAME}</li>
+                <li><a onClick={this.logoutUser}>Log out</a></li>
               </ul>
-              <form className="navbar-form navbar-right custom" role="search">
-                <div className="form-group group custom">
-                  <input type="text" placeholder="Search Tasks" />
-                  <button type="submit">
-                    <span className="glyphicon glyphicon-search"></span>
-                  </button>
-                </div>
-              </form>
             </div>
           </div>
         </nav>
