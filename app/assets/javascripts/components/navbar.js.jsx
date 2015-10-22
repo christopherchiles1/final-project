9003177a624f51c15a08d500c8f6476dbad22f2a
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-21 19:54:38 -0700

    Got dropdown to appear under navbar using some stacking-context magic in CSS

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -8,36 +8,38 @@
 
     render: function () {
       return (
-        <nav className="lt-navbar">
-          <div className="lt-navbar-left">
-            <img className="lt-logo" src="logo(small).png" />
-          </div>
-
-          <div className="lt-navbar-left">
-            <form className="lt-navbar-form">
-              <div className="lt-form-group group">
-                <input type="text" placeholder="Search Tasks" />
-                <button type="submit">
-                  <span className="glyphicon glyphicon-search"></span>
-                </button>
-              </div>
-            </form>
-          </div>
-
-          <div className="lt-navbar-right">
-            <div className="dropdown lt-navbar-dropdown">
-              <div className="dropdown-toggle"
-                data-toggle="dropdown">
-                {"Profile "}
-                <span className="caret"></span>
-              </div>
-              <ul className="dropdown-menu dropdown-menu-right">
-                <li className="dropdown-label">Logged in as {window.CURRENT_USER_USERNAME}</li>
-                <li><a onClick={this.logoutUser}>Log out</a></li>
-              </ul>
+        <div className="nav-container">
+          <nav className="lt-navbar">
+            <div className="lt-navbar-left">
+              <img className="lt-logo" src="logo(small).png" />
             </div>
-          </div>
-        </nav>
+
+            <div className="lt-navbar-left">
+              <form className="lt-navbar-form">
+                <div className="lt-form-group group">
+                  <input type="text" placeholder="Search Tasks" />
+                  <button type="submit">
+                    <span className="glyphicon glyphicon-search"></span>
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            <div className="lt-navbar-right">
+              <div className="dropdown lt-navbar-dropdown">
+                <div className="dropdown-toggle"
+                  data-toggle="dropdown">
+                  {"Profile "}
+                  <span className="caret"></span>
+                </div>
+                <ul className="dropdown-menu dropdown-menu-right">
+                  <li className="dropdown-label">Logged in as {window.CURRENT_USER_USERNAME}</li>
+                  <li><a onClick={this.logoutUser}>Log out</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
       );
     }
   });
