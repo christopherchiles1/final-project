681f1c29b0271cb48b7f4317689bfb997ea5d32b
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-23 12:12:44 -0700

    Todo form CSS and component stuff

--- app/assets/javascripts/components/todos/todo_detail.js.jsx
+++ app/assets/javascripts/components/todos/todo_detail.js.jsx
@@ -2,10 +2,35 @@
   'use strict';
 
   root.TodoDetail = React.createClass({
+    mixins: [React.addons.LinkedStateMixin],
+
+    getInitialState: function () {
+      if (this.props.todo) {
+        return { body: this.props.todo.body, completed: this.props.todo.completed };
+      } else {
+        return { body: '', completed: false };
+      }
+    },
+
+    toggleCompleted: function (e) {
+      e.preventDefault();
+      this.setState({ completed: !this.state.completed });
+    },
+
     render: function () {
+      var icon;
+      if (this.state.completed) {
+        icon = <span className="glyphicon glyphicon-ban-circle"></span>;
+      } else {
+        icon = <span className="glyphicon glyphicon-ok-circle"></span>;
+      }
+
       return (
-        <div className="todos-detail">
-          {this.props.todo.body}
+        <div className="todos-detail group">
+          <div className="todo-checkbox" onClick={this.toggleCompleted}>
+            { icon }
+          </div>
+          <input type="text" className="custom-input todo-input" valueLink={this.linkState("body")}/>
         </div>
       );
     }
